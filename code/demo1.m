b208c042131ee2e1b5e800c0f246eac60ba43f3c
sypierre <sypierrex@gmail.com>
2015-03-09 05:54:35 +0100

    4.2 - theta iterations and SEN

--- code/demo1.m
+++ code/demo1.m
@@ -10,19 +10,30 @@ q = 5;
 SIGMA = sqrt( 0.1 );
 opt.SIGMA = SIGMA;
 [Y, X, Z, BETA] = dataset_generator(N,p,q, opt);
-%% 
-opt.sigma = sqrt( rand ); % initial sigma_0
+
+%% Initial parameters:  
+   sigma = sqrt( rand ); % initial sigma_0
     beta = zeros(p,1);
     nnzeronumb = floor( .04*p );
     nnzeroentr = 8 * rand(nnzeronumb,1) + 1;
     nnzeroindx = unidrnd(p,nnzeronumb,1);% it is possible to get two equal indexes but it is very rare for large p: at least 1/p^2
     beta(nnzeroindx) = nnzeroentr;
 
-%% GradSto : sample \nabla l(\theta)
+theta = [beta; sigma]; % theta_0
 
-theta = [beta; opt.sigma]; % theta_0
+%% GradSto : sample \nabla l(\theta)
 
 for n = 1 : 1
 Nm(n) = 200+n; 
 Hnew = GradSto(Nm(n), theta, Z, X, Y);
+
+% -- compute SEN(n, beta, BETA) ---
+
+
+% -- proximal operator: P --
+
+% theta = P( theta, gamma(n+1)Hnew );
+
+
+
 end
